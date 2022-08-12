a4caaf1cb13b454567e37bef4b9cd232338dc294
rekhawb <rekha.rcr@gmail.com>
2022-08-11 23:49:34 -0400

    stripe card element addition

--- client/src/pages/DonationPage.js
+++ client/src/pages/DonationPage.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react';
-
+import {withRouter} from 'react-router-dom'
 import {Container} from '../components/styles/Container';
 import { StyledCard } from '../components/styles/Card';
 import { Flex } from'../components/styles/Flex';
@@ -9,19 +9,28 @@ import { StyledText1,StyledText2,StyledText3,animatPosition } from '../component
 import { UList} from '../components/styles/List';
 import Auth from '../utils/auth';
 import {HeaderImage} from '../components/styles/HeaderImage';
+import Card from './Card';
 
 import { loadStripe } from '@stripe/stripe-js';
-
+import {Elements,useStripe,useElements} from '@stripe/react-stripe-js'
+import {CardElement,PaymentElement} from '@stripe/react-stripe-js';
 import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../utils/queries';
 
 const stripePromise = loadStripe('pk_test_51LUT8iF2iC57NwAvWF7WbFtO6t5KiR3UD6Cud6xtgrhgtV9aK9s0BGAin1pmH1xr4sWqsoFSrBWturU6Cn4ec5Ix00DQ5gTbE6');
 
+
 const Donation = () =>{
-
+    //const stripe = useStripe();
+    //const elements = useElements();
+  
     const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+    const [getCardInfo, getCard] = useState('');
 
+    console.log(getCardInfo);
     useEffect(() => {
+
+
         if (data) {
           stripePromise.then((res) => {
             res.redirectToCheckout({ sessionId: data.checkout.session });
@@ -30,12 +39,13 @@ const Donation = () =>{
       }, [data]);
 
 
+
       function submitCheckout() {
         let donationIds = [];
      
-        donationIds = ['62f5942bc5382f2d99730d9e'];
+        donationIds = ['62f55e6bc555cdfc2d004ad7'];
 
-        alert(donationIds)
+        //alert(donationIds)
     
         getCheckout({
           variables: { donations: donationIds },
@@ -44,11 +54,12 @@ const Donation = () =>{
 
 return (
              <>
-         
+          
               <Container>
                 <Flex>
                     <StyledCard>
                        
+
                          <StyledText1>Currently supporting below listed donation campaigns</StyledText1>
                         <StyledHeader>Home for Homeless</StyledHeader>
                        <UList>
@@ -61,6 +72,28 @@ return (
                             <li>
                             Strive for safe and affordable housing for everyone
                             </li></UList>
+
+                           <StyledCard>
+
+                    <Elements stripe = {stripePromise}>
+
+<form id = "payment-form">
+
+<CardElement id="card-element" onChange={getCard}/><br></br>
+
+{Auth.loggedIn()  ? (
+                   
+                   <Button id = "btnCheckout" onClick={submitCheckout}  disabled ={!getCardInfo.complete}>Donate</Button>
+                 ) : (
+                   <StyledText2>(You are not logged in!)</StyledText2>
+                 )}
+
+
+</form>
+</Elements>
+</StyledCard>
+
+
                             <Button1>More than 326,000 people experienced sheltered homelessness in the United States on a single night in 2021.</Button1>
                             <Button1>
                             The number of sheltered individuals with chronic patterns of homelessness increased by 20 percent between 2020 and 2021.
@@ -69,17 +102,18 @@ return (
                             Veterans experiencing sheltered homelessness accounted for 11 out of every 10,000 veterans in the country.
 
                             </Button1><br></br><br></br>
-                {Auth.loggedIn() ? (
-                   
-                  <Button onClick={submitCheckout}>Donate</Button>
-                ) : (
-                  <StyledText2>(You are not logged in!)</StyledText2>
-                )}
+                             
+                          
+               
               </StyledCard>
               </Flex>
         </Container>
         </>
+
+        
       );
+
+ 
 
 };
 
